dc12360c3bf390ecf0b57fdf098f381052a4b73c
allan <epicallan.al@gmail.com>
2016-07-07 23:08:09 +0300

    quick sort

--- recursion.hs
+++ recursion.hs
@@ -15,7 +15,7 @@ maximum'' [] = error "maximum of Empty List"
 maximum'' [x] = x
 maximum'' (x:xs) = max x (maximum'' xs)
 
---- maximum [2,5,1]  = max 2 (maximum[5.1] = max 5 (maximum[1]))
+--- maximum [2,5,1]  = max 2 (maximum[5,1] = max 5 (maximum[1]))
 
 replicate' :: (Num i, Ord i) => i -> a -> [a]
 replicate' n x
@@ -42,3 +42,10 @@ elem' a [] = False
 elem' a (x: xs)
   | a == x = True
   | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+  in  smallerSorted ++ [x] ++ biggerSorted
